3fdff61db6184ec79b755bce1f25009fb42fac2a
nwelter1 <nwelter1@gmail.com>
2022-01-27 16:09:09 -0600

    LL intro final

--- linked-lists/linkedList.py
+++ linked-lists/linkedList.py
@@ -1,6 +1,9 @@
 # https://www.geeksforgeeks.org/linked-list-vs-array/
 # https://zippy-lan-200.notion.site/Intro-to-Linked-Lists-2ea969ab51874fb688d15973ffa12a61
 
+from readline import insert_text
+
+
 class Node:
     def __init__(self, value, next = None):
         self.value = value
@@ -28,29 +31,67 @@ class LinkedList:
         last = self.head
 
         while last.next:
-            print("+++++++++++++")
-            print(f'Current Node: {last}')
-            print(f'Current Value: {last.value}')
-            print(f'Current next: {last.next}')
+            # print("+++++++++++++")
+            # print(f'Current Node: {last}')
+            # print(f'Current Value: {last.value}')
+            # print(f'Current next: {last.next}')
             last = last.next
-            print('+++++++++++++')
-            print(f'New node is at:{last}')
+            # print('+++++++++++++')
+            # print(f'New node is at:{last}')
         last.next = new_node
 
+    def traverse(self):
+        if self.head is None or self.head.next is None:
+            return self.head
+
+        temp = self.head
+        while temp:
+            print(temp.value)
+            temp = temp.next
     
+    def insertAfter(self, prev_node, value):
+        if prev_node is None:
+            print('Hey, that Node doesnt exist here!')
+            return
+        
+        new_node = Node(value)
 
+        new_node.next = prev_node.next
+        prev_node.next = new_node
+
+    def removeDupsSorted(self):
+        temp = self.head
+        while temp:
+            while temp.next and temp.next.value == temp.value:
+                temp.next = temp.next.next
+            temp = temp.next
+        return self.head
     
+    def removeDups(self):
+        if self.head is None:
+            return self.head
+        values = set()
+        values.add(self.head.value)
 
+        temp = self.head
+        while temp and temp.next:
+            if temp.next.value in values:
+                temp.next = temp.next.next
+            else:
+                values.add(temp.next.value)
+                temp = temp.next
+        return self.head
 
-        
-        
 
 my_list = LinkedList()
 
 my_list.pushOn('Nate')
 my_list.pushOn("Billy")
 my_list.append('Coding Temple')
-
+my_list.traverse()
+print('______________')
+my_list.insertAfter(my_list.head.next, 'ALumni Workshop')
+my_list.traverse()
 
 
 
